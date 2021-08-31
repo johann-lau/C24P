15f9c5b90eebb72cb73cbfe19089d9aac7a6a6af
johann-lau <johannlau8888@gmail.com>
2021-08-31 17:20:26 +0800

    Updated for C25P

--- Classes.js
+++ Classes.js
@@ -56,33 +56,51 @@ class PPlayer {
     this.angle = 0;
     this.body = Bodies.rectangle(x, y, 66, 133);
     World.add(world, this.body);
-    this.arrow = Bodies.rectangle(this.x, this.y + 300, 67.8, 10, { isStatic: true });
-    World.add(world, this.arrow);
   }
 
   show() {
     push();
     var position = this.body.position
     translate(position.x, position.y);
-    rotate(-this.angle);
     image(p, 0, 0, 66, 133);
-    rotate(PI / 2);
-    image(pbow, 0, -30, 100, 100);
+    rotate(PI / 2 - this.angle);
+    image(pbow, this.angle * 10, this.angle * 40 - 30, 100, 100);
     pop();
+  }
+}
+
+class PArrow {
+  constructor(x, y, angle) {
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+    this.image = loadImage("assets/arrow.png");
+    this.body = Bodies.rectangle(this.x, this.y, 67.8, 10, { isStatic: true });
+    this.path = []
+    World.add(world, this.body);
+  }
+
+  show() {
     push();
-    var position = this.arrow.position
+    var position = this.body.position;
     translate(position.x, position.y);
-    rotate(PI - this.angle);;
-    image(arrow, 0, 0, 67.8, 10);
+    rotate(this.angle);
+    image(this.image, 0, 0, 67.8, 10);
     pop();
-    // console.log(this.arrow.position)
+    this.path.push([position.x, position.y])
+    for (var i = 0; i < this.path.length - 1; i++) {
+      ellipse(this.path[i][0], this.path[i][1], 4, 4)
+    }
+    if (position.x > 800 || position.x < -200 || position.y > 800 || position.y < -200) {
+      World.remove(world, this.body)
+      arrows.pop()
+    }
   }
 
   shoot() {
-    Body.setStatic(this.arrow, false);
-    var arrowvelocity = p5.Vector.fromAngle(PI * 1 + this.angle);
-    arrowvelocity.mult(20)
-    Body.setVelocity(this.arrow, { x: arrowvelocity.x, y: arrowvelocity.y });
-    console.log(this.arrow.velocity)
+    Body.setStatic(this.body, false);
+    var velocity = p5.Vector.fromAngle(this.angle)
+    velocity.mult(20)
+    Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
   }
 }
--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
-var engine, world;
-var canvas;
+var engine, world, canvas, DownKey;
+var arrows;
 
 function preload() {
   b1 = loadImage('assets/base1.png');
@@ -19,51 +19,59 @@ function preload() {
 function setup() {
   canvas = createCanvas(600, 600);
 
-  //Initialising Engine
   engine = Engine.create();
   world = engine.world;
 
-  //Create Player Base and Computer Base Object
   CB = new CBase(10, 450);
   CP = new CPlayer(50, 100);
   PP = new PPlayer(450, 100);
   PB = new PBase(410, 450);
+
+  DownKey = false;
+  arrows = [];
 }
 
 function draw() {
-
   background(180);
 
-  Engine.update(engine);
-
-  // Title
-  fill("#FFFF");
+  fill("#FFF");
   textAlign("center");
   textSize(40);
   text("EPIC ARCHERY", width / 2, 100);
+
+  Engine.update(engine);
 
   CB.show()
   CP.show()
   PB.show()
   PP.show()
 
-  text(mouseX+","+mouseY, mouseX, mouseY)
+  for (var i = 0; i < arrows.length; i++) {
+    arrows[i].show();
+  }
+
+  text(mouseX + "," + mouseY, mouseX, mouseY)
 }
 
 function keyPressed() {
   if (keyCode == LEFT_ARROW && PP.angle < 0.5) {
-    Matter.Body.setAngle(PP.body, PP.angle + 0.1)
-    Matter.Body.setAngle(PP.arrow, PP.angle - 0.1)
-    PP.angle = PP.angle + 0.1
+    PP.angle = PP.angle + 0.1;
   }
 
   if (keyCode == RIGHT_ARROW && PP.angle > -0.5) {
-    Matter.Body.setAngle(PP.body, PP.angle - 0.1)
-    Matter.Body.setAngle(PP.arrow, PP.angle - 0.1)
-    PP.angle = PP.angle - 0.1
+    PP.angle = PP.angle - 0.1;
   }
 
   if (keyCode == DOWN_ARROW) {
-    PP.shoot()
+    DownKey = true;
+    arrow = new PArrow(390, 390, PI - PP.angle);
+    arrows.push(arrow);
+  }
+}
+
+function keyReleased() {
+  if (keyCode == DOWN_ARROW && DownKey) {
+    DownKey = false;
+    arrows[arrows.length - 1].shoot()
   }
 }
